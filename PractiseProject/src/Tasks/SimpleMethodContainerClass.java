b98a81202055eb865f00440bdf307ddf8a2c8b49
Zsolt Filarszky <zsolt.filarszky@gmail.com>
2016-06-08 17:47:17 -0400

    SimpleMerhodContainerClass
    created a simple method container class

---
+++ PractiseProject/src/Tasks/SimpleMethodContainerClass.java
@@ -0,0 +1,47 @@
+package Tasks;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by hackerhanry on 6/8/16.
+ */
+public final class SimpleMethodContainerClass {
+
+
+    public static Integer[] CalculateTwoDegreeEquation(int A, int B, int C) {
+        /*
+        #Implementation goes here:
+        Ax^2+Bx+C=0
+        Put the solutions into an array, and give that array back as result.
+        If there is no solution for the equation throw null!
+        * */
+        throw new NotImplementedException();
+    }
+
+    public static double CalculateTwoNumbers(int firstNumber, int secondNumber, String operator){
+        /*
+        firstNumber=5
+        secondNumber=2
+        operator="*"
+        output=10
+        if operator is + the output is 7;
+        * */
+        throw new NotImplementedException();
+    }
+
+    public static boolean IsTheGivenYearLeapYear(int year){
+        /*Check if the year is leap year
+        * */
+        throw new NotImplementedException();
+    }
+
+    public static boolean IsTriangleCreatable(int a,int b, int c){
+        /*Check if the year is creatable
+        * */
+        throw new NotImplementedException();
+    }
+
+    public static int CalculateNumborOfSpecificChar(char character){
+        throw new NotImplementedException();
+    }
+}
